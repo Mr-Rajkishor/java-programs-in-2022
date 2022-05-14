3737c58aa3db5e91503123757e2eda805fe572e7
Raj kishor <rajkishor6091@gmail.com>
2022-05-15 00:13:30 +0530

    multiply two float numbers

---
+++ Q3.java
@@ -0,0 +1,10 @@
+/*                                    Program: 3
+                     Write a java program to multiply two float numbers.
+                                                                          */
+
+public class Q3 {
+    public static void main(String[] args) {
+        float a= 25.6f,b=26.23F;
+        System.out.println("Multiply of Two float number : "+a*b);
+    }
+}
