19c3532d63b49188e009a80ab00489f4090a9b48
Raj kishor <rajkishor6091@gmail.com>
2022-05-15 00:10:43 +0530

    Hello World

---
+++ Q1.java
@@ -0,0 +1,9 @@
+/*                      Program: 1
+                Write a java program to print “Hello World”.
+ */
+
+public class Q1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
