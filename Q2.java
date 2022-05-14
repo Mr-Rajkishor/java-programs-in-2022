cb9eac2d93def95d38bda73023ae8a168904857b
Raj kishor <rajkishor6091@gmail.com>
2022-05-15 00:11:40 +0530

    add two number

---
+++ Q2.java
@@ -0,0 +1,9 @@
+/*                               program: 2
+                     Write a java program to add two numbers.
+                                                                                               */
+public class Q2 {
+    public static void main(String[] args) {
+        int a=10,b=20;
+        System.out.println("Sum of two number: "+(a+b));
+    }
+}
